40b5659639787fb31f20742db93eee7102f98fee
vcoleman <nalachai@gmail.com>
2014-05-04 18:33:38 -0400

    tyring to callibrate XY servo positions

--- ArduinoMain/ArduinoMain.ino
+++ ArduinoMain/ArduinoMain.ino
@@ -1,2 +1,23 @@
-
-
+#include <Servo.h> 
+ 
+Servo Xservo; 
+Servo Yservo;  
+ 
+void setup() 
+{ 
+  Xservo.attach(8);  // attaches the servo on pin 9 to the servo object 
+  Yservo.attach(7);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+void go_to_pos(char* pos){
+  if (pos = "btm"){
+      Xservo.write(170);
+      Yservo.write(80);
+  }
+}
+ 
+void loop() 
+{ 
+  char pos[4] = "btm";
+  go_to_pos(pos);
+}
